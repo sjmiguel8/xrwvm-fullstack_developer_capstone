aff6b9923edbc0f40af78a0968aa8fb0561ec7a5
SJMiguel8 <bonilla.miguel.e@gmail.com>
2025-01-03 02:53:57 -0600

    deleted conflicting file

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-# Make migrations and migrate the database.
-echo "Making migrations and migrating the database. "
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+# Apply database migrations
+python manage.py migrate
+
+# Collect static files
 python manage.py collectstatic --noinput
+
+# Execute the command passed to docker
 exec "$@"
